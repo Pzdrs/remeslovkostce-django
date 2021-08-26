1b24f4d550abb7b6fe65ee3a67cbcef5a4f68a48
Petr <petrbohac3@seznam.cz>
2021-08-26 15:06:41 +0200

    Added ProductCategory model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.TextField()
+    thumbnail = models.ImageField
+
+    def __str__(self):
+        return self.name
+
+
