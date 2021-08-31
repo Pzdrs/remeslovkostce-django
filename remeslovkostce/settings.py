47348b81c4c6b0fccc4985bb3495060287495b1f
Petr <petrbohac3@seznam.cz>
2021-08-31 14:54:58 +0200

    Some stuff i dont even know what

--- remeslovkostce/settings.py
+++ remeslovkostce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&svq5x+cu*ip4%6*7#j5(-pjgnd09me66@c^h@k(i(h@%hbxl^'
+with open('./remeslovkostce/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
