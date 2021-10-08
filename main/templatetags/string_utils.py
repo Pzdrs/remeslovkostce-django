90354f618e2cc88d36e498acd2c93c1ecc86c920
Petr <petrbohac3@seznam.cz>
2021-10-08 15:22:25 +0200

    is_safe=True isnt working so I basically need to use the 'safe' filter

--- main/templatetags/string_utils.py
+++ main/templatetags/string_utils.py
@@ -1,11 +1,12 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
-@register.filter(is_safe=True)
+@register.filter
 def description(product, max_length):
     if len(product.description) > max_length:
-        return f'{product.description[:max_length]}...<a href="{product.get_absolute_url()}">číst dále</a>'
+        return mark_safe(f'{product.description[:max_length]}...<a href="{product.get_absolute_url()}">číst dále</a>')
     else:
         return product.description
